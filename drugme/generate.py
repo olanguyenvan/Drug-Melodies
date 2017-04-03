56c362846791ae8b0d37a1d8690ef27cecd41554
olanguyenvan <olanguyenvan@gmail.com>
2017-04-03 11:52:49 +0200

    changing options enabled

--- drugme/generate.py
+++ drugme/generate.py
@@ -5,10 +5,10 @@ from midiutil.MidiFile3 import MIDIFile
 class GenerateMelody(object):
     def __init__(self, file, pitch, length, beat_duration):
         self.pitch = pitch
-        self.length = length
+        self.length = abs(length)
         self.file = file
         self.midi_melody = MIDIFile(1)
-        self.beat_duration = beat_duration
+        self.beat_duration = abs(beat_duration)
 
     def get_filename(self):
         if not self.file.endswith(".mid"):
@@ -21,18 +21,16 @@ class GenerateMelody(object):
 
     @staticmethod
     def get_random_volume():
-        return random.randint(70, 200)
+        return random.randint(70, 127)
 
     def generate(self):
         time = 0
-        i = 0
 
         self.midi_melody.addTempo(0, time, 60)
         while time < self.length:
             self.midi_melody.addNote(0, 0, self.get_random_pitch(), time,
                                      self.beat_duration, self.get_random_volume())
             time += self.beat_duration
-            i += 1
 
         binfile = open(self.get_filename(), 'wb')
         self.midi_melody.writeFile(binfile)
--- drugme/main.py
+++ drugme/main.py
@@ -7,18 +7,34 @@ def parse_arguments():
     parser.add_argument('file', type=str, metavar='OUTPUT_FILE', help='output file name')
     parser.add_argument('--beatd', '--beat-duration', help='Set the duration of one beat', default=0.2, type=float)
     parser.add_argument('--pitch', type=int, help="Pitch value, default=50", default=50)
-    parser.add_argument('--length', type=int, help='Song\' length (seconds), default=60s', default=60)
+    parser.add_argument('--length', type=int, help='Song\'s length (seconds), default=60s', default=60)
 
     args = parser.parse_args()
     return args
 
 
+def change_arguments(args):
+    for key, item in args.items():
+        print("\nDo you want to change", key, "?")
+        yes_or_no = input("y - yes, I want to change it\nn-no, I don't want to change it\n")
+        if yes_or_no.lower() == 'y':
+            new_value = input("Type a new value\n")
+            args[key] = float(new_value)
+    return args
+
+
 def main():
     found_the_right_one = False
-    while not found_the_right_one:
-        args = parse_arguments()
+    args = parse_arguments()
+    file = args.file
+    args_dict = {
+        'pitch': args.pitch,
+        'length': args.length,
+        'beatd': args.beatd,
+    }
 
-        midi_melody = GenerateMelody(args.file, args.pitch, args.length, args.beatd)
+    while not found_the_right_one:
+        midi_melody = GenerateMelody(file, args_dict['pitch'], args_dict['length'], args_dict['beatd'])
         midi_melody.generate()
 
         print("The melody has been generated. Go to your chosen directory and tell me if you like it")
@@ -26,8 +42,9 @@ def main():
         if yes_or_no.lower() == 'y':
             found_the_right_one = True
         else:
-            print("Do you want to change any arguments?")
+            print("\nDo you want to change any arguments?")
+            yes_or_no = input("y - yes, I want to change arguments\nn - no, I don't want to change arguments")
+            if yes_or_no.lower() == 'y':
+                args_dict = change_arguments(args_dict)
 
-    print("Go and drug yourself")
-
-
+    print("\nGo and drug yourself\n")
